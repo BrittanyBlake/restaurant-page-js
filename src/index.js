c3600d3f99fdcff43cf3ae7c109ef46ab93119ba
Brittany Blake <brittany.blake49@gmail.com>
2020-08-21 13:10:41 +0200

    Add footer

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import { header } from './js/header'
 import { navbar } from "./js/navbar";
 import { home } from "./js/home";
 import { lowerhome } from "./js/lowerhome";
+import { footer } from "./js/footer";
 
 const content = document.querySelector('#content')
 content.appendChild(header);
 content.appendChild(navbar);
 content.appendChild(home);
 content.appendChild(lowerhome);
+content.appendChild(footer);

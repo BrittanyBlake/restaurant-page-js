13b1b3696344416897c7cd8af181e2074dd65034
Brittany Blake <brittany.blake49@gmail.com>
2020-08-21 13:40:15 +0200

    Add about page via dom

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { header } from './js/header'
 import { navbar } from "./js/navbar";
 import { home } from "./js/home";
 import { lowerhome } from "./js/lowerhome";
+import { about } from "./js/about"
 import { footer } from "./js/footer";
 
 const content = document.querySelector('#content')
@@ -12,4 +13,5 @@ content.appendChild(header);
 content.appendChild(navbar);
 content.appendChild(home);
 content.appendChild(lowerhome);
+content.appendChild(about);
 content.appendChild(footer);

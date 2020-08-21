0a95246674b15f740b9dce13ff425be5c1191b94
Brittany Blake <brittany.blake49@gmail.com>
2020-08-21 11:44:14 +0200

    add navbar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import "./styles/main.scss";
 // console.log("Hello webpack!")
 import { header } from './js/header'
+import { navbar } from "./js/navbar";
 
 const content = document.querySelector('#content')
 content.appendChild(header);
+content.appendChild(navbar);

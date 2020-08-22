854f1b15a37cc6e95ac37c19787b4a7e84ca6112
Brittany Blake <brittany.blake49@gmail.com>
2020-08-22 12:44:14 +0200

    change image

--- webpack.common.js
+++ webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
         use: {
           loader: 'file-loader',
           options: {
-            name: '[name].[ext]',
+            name: '[name].[hash].[ext]',
             outputPath: 'imgs',
           },
         },

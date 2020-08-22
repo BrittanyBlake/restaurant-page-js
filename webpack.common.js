613a1ca0bdd2b7ea748f2c1406844dc00376e1ad
Brittany Blake <brittany.blake49@gmail.com>
2020-08-22 12:13:17 +0200

    modify copy webpak

--- webpack.common.js
+++ webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
         use: {
           loader: 'file-loader',
           options: {
-            name: '[name].[hash].[ext]',
+            name: '[name].[ext]',
             outputPath: 'imgs',
           },
         },
